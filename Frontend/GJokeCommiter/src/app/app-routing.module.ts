5616021a2221a7c52f4a26c44d3913d3b398fff7
LuckyForce <adrian.schauer@aon.at>
2022-03-20 23:16:58 +0100

    Tried making sticky Footer

--- Frontend/GJokeCommiter/src/app/app-routing.module.ts
+++ Frontend/GJokeCommiter/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AmbienceComponent } from './ambience/ambience.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'config', component: AmbienceComponent } 
 ];
 
 @NgModule({
