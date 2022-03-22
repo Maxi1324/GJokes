ecd85808f07153527ea5ab252f6e61ece2b4e4fd
LuckyForce <adrian.schauer@aon.at>
2022-03-22 22:09:06 +0100

    Navbar changes and further acc check

--- Frontend/GJokeCommiter/src/app/header/header.component.ts
+++ Frontend/GJokeCommiter/src/app/header/header.component.ts
@@ -3,17 +3,31 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
   public href() {
     //TODO: Get Login Status
-    this.content = "Register";
-    this.link = "register";
+    let status = false;
+    this.content = 'Register';
+    this.link = 'register';
+    //Check if email and password are set
+    if (localStorage.getItem('email') && localStorage.getItem('password')) {
+      //Check if token is set
+      if (!sessionStorage.getItem('token')) {
+        //Get Token from API to check email and password.
+        this.content = 'Config';
+        this.link = 'config';
+        status = true;
+      } else status = true;
+    }
+    if (status === true) {
+      this.content = 'Config';
+      this.link = 'config';
+    }
   }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.href();
