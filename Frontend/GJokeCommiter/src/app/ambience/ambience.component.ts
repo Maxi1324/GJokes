d6db6f38531389689938fbe8b49cc151f5572d2a
LuckyForce <adrian.schauer@aon.at>
2022-03-23 22:00:08 +0100

    Config, .htaccess: Add first Config constructs and .htaccess file

--- Frontend/GJokeCommiter/src/app/ambience/ambience.component.ts
+++ Frontend/GJokeCommiter/src/app/ambience/ambience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ambience',
@@ -12,4 +12,6 @@ export class AmbienceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input()
+  public jmin: number = 0;
 }
