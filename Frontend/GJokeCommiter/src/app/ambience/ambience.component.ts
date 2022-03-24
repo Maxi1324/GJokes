b5f6abfa710f98e645cbbf9cdb53d5d4467538cb
LuckyForce <adrian.schauer@aon.at>
2022-03-24 22:17:26 +0100

    Config: Implement first save funtions and css change

--- Frontend/GJokeCommiter/src/app/ambience/ambience.component.ts
+++ Frontend/GJokeCommiter/src/app/ambience/ambience.component.ts
@@ -14,4 +14,19 @@ export class AmbienceComponent implements OnInit {
 
   @Input()
   public jmin: number = 0;
+  public jmax: number = 25;
+  public gusername: string = "";
+  public gemail: string = "";
+
+  public save_github_settings( event: Event, gusername: string, gemail: string): void {
+    console.log("Saving github settings");
+    console.log(gusername);
+    console.log(gemail);
+  }
+
+  public save_jokes_settings( event: Event, jmin: number, jmax: number): void {
+    console.log("Saving jokes settings");
+    console.log(jmin);
+    console.log(jmax);
+  }
 }
