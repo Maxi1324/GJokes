859eb4438aa50e26e2a8345257b0b10c755b30b1
maxi1234567890fischer <maxi1234567890fischer@gmail.com>
2022-05-02 11:13:33 +0200

    dings

---
+++ GStatsFaker/GStatsFaker/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using GStatsFaker.DBContexts;
+using GStatsFaker.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GStatsFaker.Controllers
+{
+    public class AdminController
+    {
+
+        public GSFContext Context { get; private set; }
+        public AdminController(GSFContext Context)
+        {
+            this.Context = Context;
+        } 
+
+        [EnableCors()]
+        [HttpPost("BloackPerson")]
+        public object BlockPerson(UserInfo UI)
+        {
+            User? u = Context.Users.SingleOrDefault((u)=> u.Id == UI.UserId);
+            if(u != null)
+            {
+
+            }
+            return null;
+        }
+    }
+}
