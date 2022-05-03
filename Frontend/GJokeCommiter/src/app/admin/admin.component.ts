d4215458600238a67c2bdd738cdef35e43f72a6c
LuckyForce <adrian.schauer@aon.at>
2022-05-03 22:29:04 +0200

    Admin: User Generation

--- Frontend/GJokeCommiter/src/app/admin/admin.component.ts
+++ Frontend/GJokeCommiter/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  //Rendered Component should update when array changes
+  //https://angular.io/guide/component-interaction#!#parent-and-children-communicate-via-a-service
+  users: User[] = [];
+
+  @ViewChild('email') email!: any;;
+  
+  constructor() {}
 
   ngOnInit(): void {
+    this.loadUsers();
   }
 
+  loadUsers() {
+    let tmp = [];
+    //TODO: Load users from backend
+    //Test Data
+    for (let i = 0; i < 10; i++) {
+      //give user random email
+      let email = "";
+      for (let j = 0; j < Math.floor(Math.random() * 10) + 5; j++) {
+        email += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+      }
+      email += "@gmail.com";
+      tmp.push(new User(i, email, new Date()));
+    }
+    this.users = tmp;
+  }
+}
+
+interface User {
+  id: number;
+  email: string;
+  joined: Date;
+}
+
+class User implements User {
+  constructor(
+    public id: number,
+    public email: string,
+    public joined: Date
+  ) {}
 }
